3ecf64c102927350f2eee049610de3be2be4cdcc
Phu Thuy <dphu569@gmail.com>
2023-03-19 18:44:18 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPlainText = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtcypher = new System.Windows.Forms.RichTextBox();
+            this.btnMahoa = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPlainText
+            // 
+            this.txtPlainText.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlainText.Location = new System.Drawing.Point(83, 162);
+            this.txtPlainText.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPlainText.Name = "txtPlainText";
+            this.txtPlainText.Size = new System.Drawing.Size(260, 121);
+            this.txtPlainText.TabIndex = 6;
+            this.txtPlainText.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 326);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Khóa K";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 136);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Bản rõ";
+            this.label1.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // txtKey
+            // 
+            this.txtKey.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKey.Location = new System.Drawing.Point(83, 345);
+            this.txtKey.Margin = new System.Windows.Forms.Padding(2);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(260, 127);
+            this.txtKey.TabIndex = 9;
+            this.txtKey.Text = "";
+            this.txtKey.TextChanged += new System.EventHandler(this.txtKey_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(527, 136);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Bản mã hóa";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtcypher
+            // 
+            this.txtcypher.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcypher.Location = new System.Drawing.Point(391, 162);
+            this.txtcypher.Margin = new System.Windows.Forms.Padding(2);
+            this.txtcypher.Name = "txtcypher";
+            this.txtcypher.Size = new System.Drawing.Size(398, 218);
+            this.txtcypher.TabIndex = 11;
+            this.txtcypher.Text = "";
+            this.txtcypher.TextChanged += new System.EventHandler(this.txtcypher_TextChanged);
+            // 
+            // btnMahoa
+            // 
+            this.btnMahoa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnMahoa.FlatAppearance.BorderSize = 0;
+            this.btnMahoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMahoa.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMahoa.Location = new System.Drawing.Point(424, 440);
+            this.btnMahoa.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMahoa.Name = "btnMahoa";
+            this.btnMahoa.Size = new System.Drawing.Size(83, 32);
+            this.btnMahoa.TabIndex = 12;
+            this.btnMahoa.Text = "Mã hóa";
+            this.btnMahoa.UseVisualStyleBackColor = false;
+            this.btnMahoa.Click += new System.EventHandler(this.btnMahoa_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(619, 440);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 32);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Giả mã";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(211, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(446, 31);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "MÃ HÓA - GIẢI MÃ AES NHÓM 17";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 505);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnMahoa);
+            this.Controls.Add(this.txtcypher);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPlainText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox txtPlainText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txtKey;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox txtcypher;
+        private System.Windows.Forms.Button btnMahoa;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using aes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMahoa_Click(object sender, EventArgs e)
+        {
+            long millisecondsStart = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            String key = txtKey.Text;
+            if (key.Length != 16)
+            {
+                MessageBox.Show("phai nhap key = 16 ky tu");
+                return;
+            }
+            Aes128Ctr aes = new Aes128Ctr(key,128);
+            String ketqua = aes.Encrypt(txtPlainText.Text);
+            long millisecondsEnd = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            MessageBox.Show($"Mã hóa thành công, thời gian mã hóa: {millisecondsEnd - millisecondsStart}(ms)");
+            txtcypher.Text = ketqua;
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                long millisecondsStart = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+                String key = txtKey.Text;
+                if (key.Length != 16 || txtcypher.Text.Length == 0)
+                {
+                    MessageBox.Show("phai nhap key = 16 ky tu");
+                    return;
+                }
+                Aes128Ctr aes = new Aes128Ctr(key, 128);
+                String ketqua = aes.Decrypt(txtcypher.Text);
+                long millisecondsEnd = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+                MessageBox.Show($"Kết quả giải mã: {ketqua}, thời gian giải mã: {millisecondsEnd-millisecondsStart}(ms)");
+            }
+            catch (Exception err)
+            {
+
+                MessageBox.Show("Giải Mã Thất Bại Vui lòng Xem lại dữ liệu đầu vào");
+            }
+            
+          
+        }
+
+        private void txtcypher_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtKey_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
